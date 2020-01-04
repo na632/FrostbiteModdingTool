2206148a3f4ed936ef95e6b44831c6578f2d44e6
Paul G <pargsoft@hotmail.com>
2020-01-04 16:19:36 +0000

    dev check in

--- FIFAModdingTests/UnitTest1.cs
+++ FIFAModdingTests/UnitTest1.cs
@@ -4,6 +4,8 @@ using System.Xml.Serialization;
 using v2k4FIFAModdingCL.Career.ObjectiveHelpers;
 using v2k4FIFAModdingCL.Career.StoryAssetHelpers;
 using FifaLibrary;
+using System.Text;
+using System.Web;
 
 namespace FIFAModdingTests
 {
@@ -60,7 +62,7 @@ namespace FIFAModdingTests
         public void TestLoadCareerSaveFile()
         {
 
-            CareerFile careerFile = new CareerFile(@"C:\Users\paula\Documents\FIFA 20\settings\Career20191107114315A", "fifa_ng_db-meta.xml");
+            CareerFile careerFile = new CareerFile(@"C:\Users\paula\Documents\FIFA 20\settings\Career20191126231940A", "fifa_ng_db-meta.xml");
             if(careerFile!=null)
             {
                 var dsets = careerFile.ConvertToDataSet();
@@ -70,6 +72,57 @@ namespace FIFAModdingTests
                 }
             }
 
+        }
+
+        [TestMethod]
+        public void TestEncryptDecrypt()
+        {
+            var readin = System.IO.File.ReadAllText(@"E:\Origin Games\FIFA 20\Patch\initfs_Win32");
+            var key = System.IO.File.ReadAllText(@"E:\Origin Games\FIFA 20\Patch\key.txt");
+            var readout = XORdecrypt(readin, key);
+            Assert.IsNotNull(readout);
+            System.IO.File.WriteAllText(@"E:\Origin Games\FIFA 20\Patch\initfs_Win32_decrypt", readout);
+
+
+        }
+
+        private static string xor(string text, string key)
+        {
+            var result = new StringBuilder();
+            for (int c = 0; c < text.Length; c++)
+                result.Append((char)((uint)text[c] ^ (uint)key[c % key.Length]));
+            return result.ToString();
+        }
+
+        public static string XORencrypt(string text, string key)
+        {
+            byte[] decrypted = Encoding.UTF8.GetBytes(text);
+            byte[] encrypted = new byte[decrypted.Length];
+
+            for (int i = 0; i < decrypted.Length; i++)
+            {
+                encrypted[i] = (byte)(decrypted[i] ^ key[i % key.Length]);
+            }
+
+            string xored = System.Convert.ToBase64String(encrypted);
+
+            return xored;
+        }
+
+        public static string XORdecrypt(string text, string key)
+        {
+            var decoded = System.Convert.FromBase64String(HttpUtility.UrlDecode(text));
+
+            byte[] result = new byte[decoded.Length];
+
+            for (int c = 0; c < decoded.Length; c++)
+            {
+                result[c] = (byte)((uint)decoded[c] ^ (uint)key[c % key.Length]);
+            }
+
+            string dexored = Encoding.UTF8.GetString(result);
+
+            return dexored;
         }
     }
 }
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using FifaLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            CareerFile careerFile = new CareerFile(@"C:\Users\paula\Documents\FIFA 20\settings\Career20191126231940A", "fifa_ng_db-meta.xml");
+            if (careerFile != null)
+            {
+                var dsets = careerFile.ConvertToDataSet();
+                if (dsets != null)
+                {
+
+                }
+            }
+        }
+    }
+}
