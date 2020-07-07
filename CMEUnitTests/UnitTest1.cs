d838b887b980c0e1c7ebc950343496630a0669de
Paul G <pargsoft@hotmail.com>
2020-07-07 12:22:18 +0100

    got the gamedb address and aob working! YES!

--- CMEUnitTests/UnitTest1.cs
+++ CMEUnitTests/UnitTest1.cs
@@ -8,14 +8,43 @@ using v2k4FIFAModdingCL.MemHack.Core;
 using CareerExpansionMod.CEM.Data;
 using CareerExpansionMod.CEM;
 using System.Threading;
+using System.Linq;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Reflection.Metadata.Ecma335;
 
 namespace CMEUnitTests
 {
+    
+
     [TestClass]
     public class UnitTest1
-
-
     {
+        //[DllImport("paulv2k4HackHelpers.dll", CallingConvention = CallingConvention.Cdecl)]
+        //public static extern ulong ResolvePtr(UIntPtr address, int offsetpos);
+
+        long ResolvePtr(UIntPtr address, int offsetpos)
+        {
+            var proc = CoreHack.GetProcess();
+            if (proc.HasValue)
+            {
+                long offset = Convert.ToInt64((address + offsetpos).ToUInt64());
+                Debug.WriteLine($"ResolvePtr:{offset.ToString("X8")}");
+                var readint = CoreHack.MemLib.readInt(offset.ToString("X8"));
+                Debug.WriteLine($"ResolvePtr:{readint.ToString("X8")}");
+
+                var addOffsetToAddress = new UIntPtr((ulong)readint + (ulong)offset + 4);
+                Debug.WriteLine($"ResolvePtr:{addOffsetToAddress.ToUInt64().ToString("X8")}");
+
+                //long resolved = 0;
+                //resolved = Convert.ToInt64((addOffsetToAddress + offsetpos + 4).ToUInt64());
+                ////return resolved;
+                //Debug.WriteLine($"ResolvePtr:{resolved.ToString("X8")}");
+
+                return (long)addOffsetToAddress.ToUInt64();
+            }
+            return 0;
+        }
 
         [TestMethod]
         public void LoadFIFAGameDBPointers()
@@ -24,10 +53,22 @@ namespace CMEUnitTests
             if(proc.HasValue)
             {
 
-                var codeGameDB_addy = CoreHack.MemLib.AoBScan("44 8B 7A 34 44 8B 62 38", true, true).Result;
-                //if(codeGameDB_addy > 0)
-                //{
-                //}
+                //var codeGameDB_addy = CoreHack.MemLib.AoBScan("44 8B 7A 34 44 8B 62 38", true, true).Result.FirstOrDefault();
+                var codeGameDB_addy = CoreHack.MemLib.AoBScan(long.Parse(CoreHack.MemLib.getMinAddress().ToString()), 0x00007ffffffeffff, "4C 0F 44 35 ?? ?? ?? ?? 41 8B 4E 08", true, true, true).Result.FirstOrDefault();
+                if (codeGameDB_addy > 0)
+                {
+                    var t2 = codeGameDB_addy.ToString("X8");
+                    if(!string.IsNullOrEmpty(t2))
+                    {
+                        Debug.WriteLine(t2);
+                        //ulong x = 0x1410FB844;
+                        var ul = ulong.Parse(codeGameDB_addy.ToString());
+                        var uintpr = new UIntPtr(ul);
+                        var newptr = ResolvePtr(uintpr, 4).ToString("X8");
+                        Debug.WriteLine(newptr);
+
+                    }
+                }
 
 
             }
