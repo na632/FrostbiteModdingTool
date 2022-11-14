7b5444044cb54052e2c1018cad1a4d6ed7720fa9
Paulov <pargsoft@hotmail.com>
2022-11-14 10:00:15 +0000

    UPD: Create properly named Riff Reader/Writer

---
+++ Libraries/FrostySdk/FrostySdk.IO/2022/EbxWriterRiff.cs
@@ -0,0 +1,19 @@
+﻿using FrostySdk.FrostySdk.IO;
+using FrostySdk.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrostySdk.FrostySdk.IO
+{
+    public class EbxWriterRiff : EbxWriter2023
+    {
+        public EbxWriterRiff(Stream inStream, EbxWriteFlags inFlags = EbxWriteFlags.None, bool leaveOpen = false)
+           : base(inStream, inFlags, true)
+        {
+        }
+    }
+}
---
+++ Libraries/FrostySdk/FrostySdk.IO/2022/Readers/EbxReaderRiff.cs
@@ -0,0 +1,20 @@
+﻿using FrostySdk.IO._2022.Readers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrostySdk.FrostySdk.IO.Readers
+{
+    public class EbxReaderRiff : EbxReader22B
+    {
+
+        public EbxReaderRiff(Stream ebxDataStream, bool inPatched)
+            : base(ebxDataStream, inPatched)
+        {
+        }
+    }
+}
+
