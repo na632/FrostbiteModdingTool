0a1b2fd6934c89919a9b4fd3b99ce91ccf3175b3
Paulov <pargsoft@hotmail.com>
2023-02-07 12:08:09 +0000

    [NEW] Start of new Project format

---
+++ Libraries/FrostySdk/ModsAndProjects/Projects/FMTProject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrostySdk.ModsAndProjects.Projects
+{
+    internal class FMTProject
+    {
+        public FMTProject() 
+        { 
+
+        }
+
+        public static FMTProject Create()
+        {
+            FMTProject project = new FMTProject();// this;
+            return project;
+        }
+
+        public static FMTProject Read(string filePath)
+        {
+            if(!File.Exists(filePath))
+                throw new FileNotFoundException(filePath);
+
+
+
+            FMTProject project = new FMTProject();
+            return project;
+        }
+
+        public FMTProject Update()
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
